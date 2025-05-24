bcb649b9f254272460b4707fda30de35c3a83005
Kokushibo <olegmark20000@gmail.com>
2025-05-24 11:59:16 +0300

    Add test commot

--- task-tracker-cli/src/main/java/com/main/Main.java
+++ task-tracker-cli/src/main/java/com/main/Main.java
@@ -1,10 +1,12 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan  = new Scanner(System.in);
 	}
 
 }
